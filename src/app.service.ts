f23c659d7bf4127e53ba869d05e8c342169329ee
Lucas Reis <l.vasconcelos@ufba.br>
2024-08-29 01:26:49 -0300

    feat: :sparkles: created the minimal backend for connection to the frontend

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+
+  async getPokemonAbilities(name: string): Promise<string[]> {
+    try {
+      const response: AxiosResponse = await this.httpService
+        .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .toPromise();
+      const abilities = response.data.abilities.map(
+        (ability) => ability.ability.name,
+      );
+      return abilities.sort();
+    } catch (err) {
+      console.error('Error fetching data from PokéAPI:', err);
+      throw new Error('Failed to fetch abilities');
+    }
   }
 }
