f390b1462a0dc8c928bd1d11e1c03dbd570b5ec8
Lucas Reis <l.vasconcelos@ufba.br>
2024-08-29 12:18:22 -0300

    refactor:

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:7000',
     methods: 'GET',
     allowedHeaders: 'Content-Type, Accept',
   });
